01ad6fac5cc2113945937d6c3ed8874d5c3aaa48
jetyue <jetyue777@gmail.com>
2017-10-15 13:52:09 -0400

    second commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello add, %s!", someone);
+    return String.format("Hello add jet, %s!", someone);
   }
 }
